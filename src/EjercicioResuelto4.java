b506f11e04fd4e5a50eff425b08cee60e815a436
SAMUEL <SAMUEL@LAPTOP-RQ08L5ED>
2023-08-30 21:20:52 -0500

    Ejercicio Resuelto No4

---
+++ src/EjercicioResuelto4.java
@@ -0,0 +1,21 @@
+
+public class EjercicioResuelto4 {
+
+	public static void main(String[] args) {
+		
+		int juan, alberto, ana, mama;
+		
+		juan = 9;
+		alberto = (2/3)*juan;
+		ana = (4/3)*juan;
+		mama = juan + alberto + ana;		
+		
+		System.out.println("Las edades son:");
+		System.out.println("Alberto: "+ alberto);
+		System.out.println("Juan: "+ juan);
+		System.out.println("Ana: "+ana);
+		System.out.println("Mama: "+ mama);
+				
+	}
+
+}
